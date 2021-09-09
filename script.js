2ec79018a6c4e4bc8d35126c89f1c2169c42891f
Brayan Maximiano <brayanmaximiano01@gmail.com>
2021-09-09 19:25:10 -0300

    Adicionado aba Curriculo e formulario pelo netlify

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ document.querySelector(".hamburguer").addEventListener("click", ()=>{
     document.querySelector(".container").classList.toggle("show-menu")
 });
 
-//    Evento no botao "saiba Mais do banner"
-document.querySelector("#btn1").addEventListener("click", ()=>{
-    open("index.html", "_self")
+
+let botao_curriculo = document.querySelector("curriculo")
+ 
+function curriculo(){
+    open("saibamais.html","_self")
+}
+
+document.querySelector("#proj1").addEventListener("click", ()=>{
+    alert("Essa Funcao esta temporariamente desabilitada")
+})
+document.querySelector("#proj2").addEventListener("click", ()=>{
+    alert("Essa Funcao esta temporariamente desabilitada")
+})
+document.querySelector("#proj3").addEventListener("click", ()=>{
+    alert("Essa Funcao esta temporariamente desabilitada")
+})
+document.querySelector("#more-proj").addEventListener("click", ()=>{
+    alert("Essa Funcao esta temporariamente desabilitada")
 })
