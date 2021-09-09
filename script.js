d5553831d6f8f968a0221b24df910ab252f6393a
Brayan Maximiano <brayanmaximiano01@gmail.com>
2021-09-09 16:22:22 -0300

    adicionado footer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.querySelector(".hamburguer").addEventListener("click", ()=>{
     document.querySelector(".container").classList.toggle("show-menu")
 });
+
+//    Evento no botao "saiba Mais do banner"
+document.querySelector("#btn1").addEventListener("click", ()=>{
+    open("index.html", "_self")
+})
